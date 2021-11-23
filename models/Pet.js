23a505dd13ae829aa65aadd2cde5d2bd5d7cd4b4
Paige A <pvaltemare@gmail.com>
2021-11-22 16:52:16 -0800

    add test models

---
+++ models/Pet.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our Post model
+class Pet extends Model {}
+
+// create fields/columns for Post model
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'pet'
+    }
+);
+
+module.exports = Pet;
